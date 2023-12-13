4459cb5b0ba9ab53c0cb1d12ab1e8753e23ff7d3
stephxho <149762989+stephxho@users.noreply.github.com>
2023-12-13 11:54:38 +0100

    Updated JS file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-function search(event) {
-  function showCurrentWeather(response) {
-    let temperature = Math.round(response.data.temperature.current);
-    let currentTemperature = document.querySelector("#current-temperature");
-    currentTemperature.innerHTML = temperature;
-  }
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
 
+function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
+  let city = searchInputElement.value;
 
-  let apiKey = "8b568tdd52ea0467a4ff3f2cbo8f1f31";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}&units=metric`;
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(showCurrentWeather);
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function formatDate(date) {
